0264d58235fe136e1f5370fcd105fb1c4cd06743
Joao Vitor Mocambite Lima <86741407+JoaoVitor2022dev@users.noreply.github.com>
2023-03-15 18:16:20 -0400

    Create Address.js

---
+++ models/Address.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+
+const db = require('../db/conn');
+
+const User = require('./User');
+
+const Adress = db.define('Adress', { 
+ 
+    street: {
+        type: DataTypes.STRING,
+        required: true,
+    },
+    number: {
+        type: DataTypes.STRING,
+        required: true,
+    },
+    city: {
+        type: DataTypes.STRING,
+        required: true,
+    },   
+}); 
+
+// relacionamento 
+
+Adress.belongsTo(User);
+
+module.exports = Adress;
